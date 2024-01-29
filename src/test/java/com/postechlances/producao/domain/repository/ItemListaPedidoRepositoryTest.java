2c6c7889c82b4afc028de8c188cdc13785de6710
DaniloParkour <DaniloParkour>
2024-01-28 22:07:11 -0300

    test 2

--- src/test/java/com/postechlances/producao/domain/repository/ItemListaPedidoRepositoryTest.java
+++ src/test/java/com/postechlances/producao/domain/repository/ItemListaPedidoRepositoryTest.java
@@ -9,9 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import java.sql.Timestamp;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -41,19 +40,10 @@ public class ItemListaPedidoRepositoryTest {
   void deveRegistrarUmNovoPedido() {
 
     //ARRANJE
-    var itemLista = new ItemListaPedido();
-    itemLista.setIdentifier_pedido("1");
-    itemLista.setIdentifier_cliente("10");
-    itemLista.setRecebimento(new Timestamp(new Date().getTime()));
-    itemLista.setStatus(StatusPedido.SOLICITADO);
-    List<String> items = new ArrayList<String>();
-    items.add("Cheese Burguer");
-    items.add("Batata G c/ Bacon");
-    items.add("Sorvete de manga com cobertura de menta");
-    items.add("Coca Zero 1,5L");
-    items.add("Eno Guaraná");
-    itemLista.setItens(items);
+    var itemLista = criarItemPedido();
 
+    // Mockando o banco, quando enviado um save para o repository passando como parâmetro um objeto da classe
+    // ItemListaPedido.class, retornar o objeto itemLista
     when(repository.save(any(ItemListaPedido.class))).thenReturn(itemLista);
 
     //ACT
@@ -77,7 +67,31 @@ public class ItemListaPedidoRepositoryTest {
 
   @Test
   void deveDetalharUmPedido() {
-    fail("Teste não implementado!");
+    //ARRANJE
+    Long id = new Random().nextLong();
+    ItemListaPedido item = criarItemPedido();
+    item.setId(id);
+
+    // Mocka para quando pesquisar pelo ID retornar o item definido acima
+    when(repository.findById(any(Long.class))).thenReturn(Optional.of(item));
+
+    //ACT
+    var optionalItem = repository.findById(id);
+
+    //ASSET
+    verify(repository, times(1)).findById(id);
+    assertThat(optionalItem).isPresent().containsSame(item);
+
+    optionalItem.ifPresent(savedItem -> {
+      assertThat(savedItem.getId()).isEqualTo(item.getId());
+      assertThat(savedItem.getIdentifier_pedido()).isEqualTo(item.getIdentifier_pedido());
+      assertThat(savedItem.getIdentifier_cliente()).isEqualTo(item.getIdentifier_cliente());
+      assertThat(savedItem.getStatus()).isEqualTo(item.getStatus());
+      assertThat(savedItem.getRecebimento()).isEqualTo(item.getRecebimento());
+      assertThat(savedItem.getPreparo()).isEqualTo(item.getPreparo());
+      assertThat(savedItem.getFechamento()).isEqualTo(item.getFechamento());
+      assertThat(savedItem.getItens()).isEqualTo(item.getItens());
+    });
   }
 
   @Test
@@ -88,6 +102,22 @@ public class ItemListaPedidoRepositoryTest {
   @Test
   void deveAvancarStatusDoPedido  () {
     fail("Teste não implementado!");
+  }
+
+  private ItemListaPedido criarItemPedido() {
+    var itemLista = new ItemListaPedido();
+    itemLista.setIdentifier_pedido("1");
+    itemLista.setIdentifier_cliente("10");
+    itemLista.setRecebimento(new Timestamp(new Date().getTime()));
+    itemLista.setStatus(StatusPedido.SOLICITADO);
+    List<String> items = new ArrayList<String>();
+    items.add("Cheese Burguer");
+    items.add("Batata G c/ Bacon");
+    items.add("Sorvete de manga com cobertura de menta");
+    items.add("Coca Zero 1,5L");
+    items.add("Eno Guaraná");
+    itemLista.setItens(items);
+    return itemLista;
   }
 
 }
